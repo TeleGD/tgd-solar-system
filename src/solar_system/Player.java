d877e378ef169df249b61ebe518b379c86e5a429
Maxime-Daalimar <43240772+Maxime-Daalimar@users.noreply.github.com>
2018-10-18 15:31:58 +0200

    Create Player.java
    Oui Voilà on a une classe Player. Yes.

---
+++ src/solar_system/Player.java
@@ -0,0 +1,24 @@
+package solar_system;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Player {
+	private Map<String , Integer> ressources = new HashMap<String,Integer>(); 
+	private int [] technologie = new int[2];
+	private List<Planet> knownPlanets; 
+	
+	// initialiZation des ressources du joueur
+	
+	//ressources.put("Nourriture",0);
+
+
+public void addRessource(String type , int augmentation ){
+	if(type == "cobble"){
+		ressource[0] += augmentation;
+	}
+	else if(type == "Mana")
+}
+
+}
