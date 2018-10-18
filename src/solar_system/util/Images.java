75c62cb90720d70a58d6048ddca7365d22e853e1
Quentin Charrier <quentin.charrier@telecomnancy.net>
2018-10-18 16:52:10 +0200

    Image de Satellite

--- src/solar_system/util/Images.java
+++ src/solar_system/util/Images.java
@@ -8,11 +8,14 @@ import org.newdawn.slick.SlickException;
 public class Images {
 	
 	private static ArrayList<Image> planet = new ArrayList<Image>();
+	private static ArrayList<Image> satellite = new ArrayList<Image>();
 	
 	static {
 		try {
 			for (int i=0; i<5; i++) {
 				planet.add(new Image("res/images/planet/planet"+i+".png"));
+				satellite.add(new Image("res/images/satellite/satellite"+i+".png"));
+				
 			}
 		} catch (SlickException e) {
 			e.printStackTrace();
@@ -21,5 +24,9 @@ public class Images {
 		
 	public static Image getPlanet(int i) {
 		return planet.get(i);
+	}
+
+	public static Image getSatellite(int i) {
+		return satellite .get(i);
 	}
 }
