ae08740300909a2926c0a4e752278d2986fb983b
Océane Chazé <oceane.chaze@telecomnancy.eu>
2018-11-08 16:43:02 +0100

    Utilisation de la classe Resource dans le Player

--- src/solar_system/Player.java
+++ src/solar_system/Player.java
@@ -10,7 +10,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Player {
-	private Map<String , Double> ressources = new HashMap<String,Double >();
+	private Map<String , Resource> ressources = new HashMap<String,Resource >();
 	private int [] technologie = new int[2];
 	private List<Planet> knownPlanets;
 	private int DiscoveredPlanet;
@@ -20,11 +20,10 @@ public class Player {
 	// initialiZation des ressources du joueur
 	
 	public Player(World world) {
-		ressources.put("Nourriture",0.);
-		ressources.put("Fer",10.);
-		ressources.put("Noyau Linux",1.);
+		ressources.put("Nourriture",new Resource("Nourriture"));
+		ressources.put("Fer",new Resource("Fer"));
+		ressources.put("Noyau Linux", new Resource("Noyau Linux"));
 		this.world=world;
-		
 	}
 	
 
@@ -34,9 +33,9 @@ public class Player {
 		size=ressources.size();//taille du hashmap
 		context.setColor(Color.black);
 		context.fillRect(0, 0,world.getWidth(),world.getHeight()/20);
-		for(Map.Entry<String , Double> ressource: ressources.entrySet()){
+		for(Map.Entry<String , Resource> resource: ressources.entrySet()){
 			context.setColor(Color.white);
-			context.drawString(ressource.getKey()+" : "+ressource.getValue(), world.getWidth()*comp/size, world.getHeight()/40);//Essai d'affichage des ressources :reste à miodif l'emplacement de chaque(pas 399,10));
+			context.drawString(resource.getKey()+" : "+resource.getValue().getQuantite(), world.getWidth()*comp/size, world.getHeight()/40);//Essai d'affichage des ressources :reste à miodif l'emplacement de chaque(pas 399,10));
 			comp+=1;
 		}
 		
