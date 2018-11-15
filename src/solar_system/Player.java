75a195ea5bdeb03f63618190b9e3663fa2c16124
Océane Chazé <oceane.chaze@telecomnancy.eu>
2018-11-15 16:46:49 +0100

    Modifications du Player (plus propre)

--- src/solar_system/Player.java
+++ src/solar_system/Player.java
@@ -10,7 +10,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Player {
-	private Map<String , Resource> ressources = new HashMap<String,Resource >();
+	private Map<String , Resource> resources = new HashMap<String,Resource >();
 	private int [] technologie = new int[2];
 	private List<Planet> knownPlanets;
 	private int DiscoveredPlanet;
@@ -20,9 +20,8 @@ public class Player {
 	// initialiZation des ressources du joueur
 	
 	public Player(World world) {
-		ressources.put("Nourriture",new Resource("Nourriture"));
-		ressources.put("Fer",new Resource("Fer"));
-		ressources.put("Noyau Linux", new Resource("Noyau Linux"));
+		initRessources();
+		getResources();
 		this.world=world;
 	}
 	
@@ -30,10 +29,10 @@ public class Player {
 	public void render(GameContainer container, StateBasedGame game, Graphics context){
 		int size;
 		int comp=0;
-		size=ressources.size();//taille du hashmap
+		size=resources.size();//taille du hashmap
 		context.setColor(Color.black);
 		context.fillRect(0, 0,world.getWidth(),world.getHeight()/20);
-		for(Map.Entry<String , Resource> resource: ressources.entrySet()){
+		for(Map.Entry<String , Resource> resource: resources.entrySet()){
 			context.setColor(Color.white);
 			context.drawString(resource.getKey()+" : "+resource.getValue().getQuantite(), world.getWidth()*comp/size, world.getHeight()/40);//Essai d'affichage des ressources :reste à miodif l'emplacement de chaque(pas 399,10));
 			comp+=1;
@@ -46,10 +45,18 @@ public class Player {
 		
 	}
 	
+	public void initRessources(){
+			resources.put("Nourriture",new Resource("Nourriture"));
+			resources.put("Fer",new Resource("Fer"));
+			resources.put("Noyau Linux", new Resource("Noyau Linux"));
+		}
+	public Map<String,Resource> getResources(){
+		return resources;
+	}
 	
 	public void addRessource(String type , double augmentation ){
-		if(ressources.containsValue(type)){
-		ressources.put(type,ressources.get(type)); // + augmentation	
+		if(resources.containsValue(type)){
+		resources.put(type,resources.get(type)); // + augmentation	
 		// Fabien nous garantit que ca ne va pas merder.  
 	
 		}
