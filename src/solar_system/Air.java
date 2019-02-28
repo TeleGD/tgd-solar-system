75461a4b82239f0ebc1d4695ac37cb487c97b451
Océane Chazé <oceane.chaze@telecomnancy.eu>
2019-02-28 16:55:31 +0100

    Ajout du mousepressed dans Orbital.java(détecte si une orbitale a été cliquée) mais non appelé ailleurs).

--- src/solar_system/Air.java
+++ src/solar_system/Air.java
@@ -13,6 +13,7 @@ import org.newdawn.slick.state.transition.FadeOutTransition;
 public class Air {
 	private int nbOrbitaux;
 	private int distance;
+	private World world;
 	
 	private List<Orbital> orbitals;
 	
@@ -20,6 +21,26 @@ public class Air {
 		this.nbOrbitaux = nbOrbitaux;
 		this.distance = distance;
 		this.orbitals = new ArrayList<Orbital>();
+	}
+	
+	public int mousePressed(int arg0,int x ,int y) { 
+		// padding = marge intérieure (distance entre la grille et le bord de l'image)
+		float facteur_magique = (float)(this.world.getHeight())/1080;
+		int sizeCase = Math.round(80*facteur_magique);
+		
+		for(int i=0;i<orbitals.size();){
+			//Correspond aux coordonnées du coin en haut à gauche de la case sur le satellite ou la station.
+			int coin_x = Math.round(orbitals.get(i).get_x()+orbitals.get(i).get_size()-sizeCase);
+			int coin_y = Math.round(orbitals.get(i).get_y()+orbitals.get(i).get_size()-sizeCase);
+			if(x>coin_x || x<coin_x+sizeCase || y>coin_y || y<coin_y+sizeCase){
+				System.out.println("Tu as cliqué sur le satellite !");
+				return i;				
+			}
+		}
+		System.out.println("Oh non, tu as cliqué à côté du satellite...");
+		return -1;
+
+		
 	}
 	
 	public void addOrbital(Orbital o) {
